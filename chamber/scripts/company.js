4af821aafc89411cef406559aacb60a1085b8328
mcdonaldmark <markamcdonald7@gmail.com>
2025-05-14 11:13:52 -0600

    sync

--- chamber/scripts/company.js
+++ chamber/scripts/company.js
@@ -1,26 +1,31 @@
-const url = 'https://mcdonaldmark.github.io/wdd231/chamber/data/members.json';
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
-async function getCompanyData() {
+async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.companies);
-    displayCompanies(data.companies);
+    console.table(data.prophets);
+    displayProphets(data.prophets);
 }
 
-getCompanyData();
+getProphetData();
 
-const displayCompanies = (companies) => {
-    companies.forEach((company) => {
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
         let card = document.createElement('section');
-        let name = document.createElement('h2'); // fill in the blank
-
+        let fullName = document.createElement('h2'); // fill in the blank
+        let portrait = document.createElement('img');
         // Build the h2 content out to show the prophet's full name
-        name.textContent = `${company.name}`; // fill in the blank
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
         // Build the image portrait by setting all the relevant attributes
-
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
 
         // Append the section(card) with the created elements
-        card.appendChild(name); //fill in the blank
+        card.appendChild(fullName); //fill in the blank
+        card.appendChild(portrait);
 
         cards.appendChild(card);
     }); // end of arrow function and forEach loop
