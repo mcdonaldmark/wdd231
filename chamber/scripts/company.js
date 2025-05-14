a774cb0c1243dc583f18618d962f18514027a2c6
mcdonaldmark <markamcdonald7@gmail.com>
2025-05-14 11:18:28 -0600

    sync

--- chamber/scripts/company.js
+++ chamber/scripts/company.js
@@ -1,4 +1,4 @@
-const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const url = 'https://mcdonaldmark.github.io/wdd231/chamber/data/members.json';
 const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch(url);
@@ -13,19 +13,11 @@ const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         let card = document.createElement('section');
         let fullName = document.createElement('h2'); // fill in the blank
-        let portrait = document.createElement('img');
-        // Build the h2 content out to show the prophet's full name
-        fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
-        // Build the image portrait by setting all the relevant attributes
-        portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+
+        fullName.textContent = `${prophet.name}`;
 
         // Append the section(card) with the created elements
         card.appendChild(fullName); //fill in the blank
-        card.appendChild(portrait);
 
         cards.appendChild(card);
     }); // end of arrow function and forEach loop
