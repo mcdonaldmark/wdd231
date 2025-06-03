fbac6e194af3320174546583e499c057fa90cce3
mcdonaldmark <markamcdonald7@gmail.com>
2025-06-02 23:51:03 -0600

    sync

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,4 +1,4 @@
-const url = 'https://mcdonaldmark.github.io/wdd231/chamber/data/members.json';
+const url = 'https://mcdonaldmark.github.io/wdd231/chamber/data/attractions.json';
 const cards = document.querySelector('#cards');
 async function getCompanyData() {
     const response = await fetch(url);
