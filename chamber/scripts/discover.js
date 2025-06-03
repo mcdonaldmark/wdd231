7760465d865b1771a979e7901155d68f59e4666c
mcdonaldmark <markamcdonald7@gmail.com>
2025-06-02 23:50:12 -0600

    sync

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,6 @@
+const url = 'https://mcdonaldmark.github.io/wdd231/chamber/data/members.json';
+const cards = document.querySelector('#cards');
+async function getCompanyData() {
+    const response = await fetch(url);
+    const data = await response.json();
+}
