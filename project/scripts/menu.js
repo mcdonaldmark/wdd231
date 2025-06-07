3d9dc2d263a02b440f6409402417f15abf26ddd8
mcdonaldmark <markamcdonald7@gmail.com>
2025-06-07 13:09:01 -0600

    sync

--- project/scripts/menu.js
+++ project/scripts/menu.js
@@ -1,27 +1,40 @@
-const products = [
-    {
-        id: "1",
-        name: "Cheeseballs with Strawberry Smoothie",
-    },
-    {
-        id: "2",
-        name: "Bolon",
-    },
-    {
-        id: "3",
-        name: "Chicken Soup with Egg Noodles",
-    },
-    {
-        id: "4",
-        name: "Lomo Saltado",
-    },
-];
+const url = 'https://mcdonaldmark.github.io/wdd231/project/data/cuisine.json';
+const cards = document.querySelector('#cards');
+async function getCuisineData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayCuisine(data.cuisine);
+}
 
-const selectElement = document.getElementById("productName");
+getCuisineData();
 
-products.forEach(product => {
-    const optionElement = document.createElement("option");
-    optionElement.value = product.id;
-    optionElement.textContent = product.name;
-    selectElement.appendChild(optionElement);
-});
+const displayCuisine = (cuisine) => {
+    cuisine.forEach((dish) => {
+
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let image = document.createElement('img');
+        let category = document.createElement("p");
+        let description = document.createElement('p');
+        let button = document.createElement('button');
+
+        title.textContent = `${dish.name}`;
+        image.setAttribute('src', attraction.image);
+        image.setAttribute('alt', `${title}`);
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '300');
+        image.setAttribute('height', '200');
+        category.textContent = `${dish.category}`;
+        description.textContent = `Description: ${dish.description}`;
+        button.textContent = `Learn More`;
+
+        card.appendChild(name);
+        card.appendChild(image);
+        card.appendChild(category);
+        card.appendChild(description);
+        card.appendChild(button);
+
+        document.querySelector(".grid1").appendChild(card);
+    }
+    );
+}
