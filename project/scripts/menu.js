5c82b1465c6135501a3d6ab6802c34c6210e476f
mcdonaldmark <markamcdonald7@gmail.com>
2025-06-07 16:23:50 -0600

    sync

--- project/scripts/menu.js
+++ project/scripts/menu.js
@@ -3,38 +3,31 @@ const cards = document.querySelector('#cards');
 async function getCuisineData() {
     const response = await fetch(url);
     const data = await response.json();
+    console.table(data.cuisine);
     displayCuisine(data.cuisine);
 }
 
 getCuisineData();
 
 const displayCuisine = (cuisine) => {
-    cuisine.forEach((dish) => {
-
+    cuisine.forEach((item) => {
         let card = document.createElement('section');
         let name = document.createElement('h2');
-        let image = document.createElement('img');
-        let category = document.createElement("p");
+        let portrait = document.createElement('img');
         let description = document.createElement('p');
-        let button = document.createElement('button');
 
-        title.textContent = `${dish.name}`;
-        image.setAttribute('src', attraction.image);
-        image.setAttribute('alt', `${title}`);
-        image.setAttribute('loading', 'lazy');
-        image.setAttribute('width', '300');
-        image.setAttribute('height', '200');
-        category.textContent = `${dish.category}`;
-        description.textContent = `Description: ${dish.description}`;
-        button.textContent = `Learn More`;
+        name.textContent = `Company Name: ${item.name}`;
+        description.textContent = `${item.description}`;
+        portrait.setAttribute('src', item.image);
+        portrait.setAttribute('alt', `${item.name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '250');
+        portrait.setAttribute('height', '240');
 
         card.appendChild(name);
-        card.appendChild(image);
-        card.appendChild(category);
+        card.appendChild(portrait);
         card.appendChild(description);
-        card.appendChild(button);
 
-        document.querySelector(".grid1").appendChild(card);
-    }
-    );
+        cards.appendChild(card);
+    });
 }
